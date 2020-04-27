4822537787a8ce9b57402a9ae0d101d3cef7d0f2
Arthur Verrept <44362029+ArthurVerrept@users.noreply.github.com>
2020-04-27 12:29:16 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    MongoURI: 'mongodb+srv://admin:Av494847@moviequote-kwsqp.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://admin:@moviequote-kwsqp.mongodb.net/test?retryWrites=true&w=majority'
 }
