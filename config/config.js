18ed159a878b56149a93e2174508ef9b57c642a3
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2020-05-27 13:46:10 +0100

    make it better

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports={
+    CLOUD_NAME: 'db8tnyh6d', 
+    API_KEY: '248926724177372',
+    API_SECRET: 'W4H9l9aVuOpep2jvHI0Zhz7zB3c'
+}
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    MongoURI: 'mongodb+srv://admin:Av494847@moviequote-kwsqp.mongodb.net/test?retryWrites=true&w=majority'
+}
